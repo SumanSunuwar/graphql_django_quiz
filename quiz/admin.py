8807df67b2f1b808c5a68003835d7c35304327ef
Suman <72369336+SumanSunuwar@users.noreply.github.com>
2022-09-19 16:17:14 +0545

    added quiz model, integrated with graphql for queries

---
+++ quiz/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+@admin.register(models.Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+
+@admin.register(models.Quizzes)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+    ]
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = models.Answer
+    fields = [
+        'answer_text',
+        'is_right'
+    ]
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'quiz',
+        ]
+    inlines = [
+        AnswerInlineModel,
+        ]
+
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        'answer_text',
+        'is_right',
+        'question'
+    ]
